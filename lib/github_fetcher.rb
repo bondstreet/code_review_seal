0cdddfffab2fab369caaa55742342121264d8d13
Katrina Ellison Geltman <katrina@bondstreet.com>
2017-04-08 17:27:59 -0400

    Stop excluding PRs by "unsubscribed people"

--- lib/github_fetcher.rb
+++ lib/github_fetcher.rb
@@ -82,13 +82,13 @@ class GithubFetcher
     key = "#{ORGANISATION}/#{repo}/#{pull_request.number}".to_sym
     reviewers = @github.get("repos/#{ORGANISATION}/#{repo}/pulls/#{pull_request.number}/requested_reviewers")
     reviewers.map { |reviewer| reviewer.login }
+
   end
 
   def hidden?(pull_request, repo)
     excluded_repo?(repo) ||
       excluded_label?(pull_request, repo) ||
-      excluded_title?(pull_request.title) ||
-      !person_subscribed?(pull_request)
+      excluded_title?(pull_request.title)
   end
 
   def excluded_label?(pull_request, repo)
